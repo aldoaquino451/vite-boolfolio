427f599e8cf9d6e6461b9524983e832e94d45075
aldoaquino451 <aldoaquino2909@gmail.com>
2023-12-06 17:18:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import axios from 'axios';
+import { store } from './data/store';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      store,
+      projects: []
+    }
+  },
+  methods: {
+    getApi() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then( res => {
+          console.log(res.data.data);  
+          this.projects = res.data.data;
+        })
+    }
+  },
+  mounted() {
+    this.getApi();
+  },
+
+}
+</script>
+
+<template>
+
+  <header>
+    <h2>Header</h2> 
+  </header>
+
+  <main>
+    <div class="container">
+      <h1>Welcome to Vue</h1>
+      <ul>
+        <li v-for="project in projects" :key="project.id" >
+          <strong>Nome progetto: </strong> {{ project.name }} <br>
+          <strong>Data: </strong> {{ project.date }} <br>
+          <strong>Descrizione: </strong> {{ project.description }}   
+        </li>
+      </ul> 
+    </div>
+  </main>
+
+</template>
+
+<style lang="scss">
+
+</style>
